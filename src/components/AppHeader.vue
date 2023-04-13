29692e12f343c64985d2a09fb49d248c75a08fd0
filecc <fil.gia@live.it>
2023-04-13 14:25:17 +0200

    header set

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,35 @@
+<template>
+    <header class="row justify-content-between align-items-center h-100">
+        <div class="col-2 p-2">
+            <img class="img-fluid" src="../assets/img/dc-logo.png" alt="">
+        </div>
+        <div class="col-10 p-2 d-flex justify-content-end h-100">
+            <ul class="d-flex justify-content-center gap-3 list-unstyled flex-wrap m-0 p-0 h-100">
+                <li class="text-uppercase" v-for="link in links" :key="link.id">
+                    {{ link.text }}
+                </li>
+            </ul>
+        </div>
+    </header>
+</template>
+
+<script>
+import headerLinks from '../data/data.js'
+export default {
+    data() {
+        return {
+            links: headerLinks
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    img{
+        max-width: 50px;
+        margin: 0 auto;
+    }
+    li{
+        font-size: 10px;
+    }
+</style>
---
+++ src/data/data.js
@@ -0,0 +1,43 @@
+const headerLinks = [
+        {
+            "id": "1",
+            "text": "characters"
+        },
+        {
+            "id": "2",
+            "text": "comics"
+        },
+        {
+            "id": "3",
+            "text": "movies"
+        },
+        {
+            "id": "4",
+            "text": "tv"
+        },
+        {
+            "id": "5",
+            "text": "games"
+        },
+        {
+            "id": "6",
+            "text": "collectibles"
+        },
+        {
+            "id": "7",
+            "text": "videos"
+        },
+        {
+            "id": "8",
+            "text": "fans"
+        },
+        {
+            "id": "9",
+            "text": "news"
+        },
+        {
+            "id": "10",
+            "text": "shop"
+        }
+    ]
+export default headerLinks
